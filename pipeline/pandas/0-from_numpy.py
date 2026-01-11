1b2d82065c17d3ed2db1313dc466b8e7602e986e
kanangoylarov <goylerov03@gmail.com>
2026-01-11 14:52:10 +0000

    go to heaven

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -6,10 +6,11 @@ This is creating pandas dataframe from numpy array function file
 import pandas as pd
 
 def from_numpy(array):
+
     '''
     Creating function for process
     '''
 
     order = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
-    df = pd.DataFrame(array, columns = order[:array.shape[1]])
+    df = pd.DataFrame(array, columns=order[:array.shape[1]])
     return df
