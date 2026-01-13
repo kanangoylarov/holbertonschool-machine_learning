7ff87782d3bc0f84440637a091fc3e60b962b576
kanangoylarov <goylarov03@gmail.com>
2026-01-13 16:14:38 +0000

    new and full code

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -8,7 +8,7 @@ def summation_i_squared(n):
     """
     thsi is my function
     """
-    if type(n) == int:
-        return n*(n + 1)*(2*n + 1)
+    if type(n) == int and i > 0:
+        return n*(n + 1)*(2*n + 1) / 6
     else:
         return None
