f64017ccd6d790b395147c99bf8bc98eb6e9ab28
kanangoylarov <goylerov03@gmail.com>
2026-01-14 14:28:43 +0000

    my tenth commit

--- math/calculus/10-mattisse.py
+++ math/calculus/10-mattisse.py
@@ -5,6 +5,23 @@ this is my function doc for entry
 
 
 def poly_derivative(poly):
-    """ this is my function doc"""
-    if isinstance(poly, int):
+    """Return the derivative of a polynomial represented as a list of coefficients."""
+    
+    # Validate input
+    if not isinstance(poly, list) or len(poly) == 0:
+        return None
+    
+    for coef in poly:
+        if not isinstance(coef, (int, float)):
+            return None
 
+    # Compute derivative
+    result = []
+    for i in range(1, len(poly)):
+        result.append(poly[i] * i)
+
+    # If derivative is zero
+    if len(result) == 0 or all(coef == 0 for coef in result):
+        return [0]
+
+    return result
