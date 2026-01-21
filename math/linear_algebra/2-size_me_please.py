c34feb617a09c3f5f072abbc6da30a75a81cd1de
kanangoylarov <goylerov03@gmail.com>
2026-01-21 06:55:48 +0000

    i dont know how it work

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
-"""docs"""
-import numpy as np
+''' function that calculates the shape of a matrix'''
+
 
 def matrix_shape(matrix):
-    """doc"""
-    matrix1 = np.array(matrix)
-    return matrix1.shape
+    ''' function that calculates the shape of a matrix'''
+    shape = []
+
+    while type(matrix) is list:
+        '''size of dimension'''
+        shape.append(len(matrix))
+        '''move to next one'''
+        if matrix[0]:
+            matrix = matrix[0]
+        else:
+            break
+    return shape
