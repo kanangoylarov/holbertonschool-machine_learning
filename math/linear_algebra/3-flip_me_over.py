8aee735885ee1884fb3f898ce76eda6432520773
kanangoylarov <goylerov03@gmail.com>
2026-01-21 07:10:39 +0000

    update for flip me

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""docs"""
+
 def matrix_transpose(matrix):
     """Return the transpose of a matrix"""
     tras = []
@@ -7,6 +9,6 @@ def matrix_transpose(matrix):
 
     for i in range(cols):
         new = []
-        new.append([matrix[j][i] for j in range(rows)])
+        new = [matrix[j][i] for j in range(rows)]
         tras.append(new)
     return tras
