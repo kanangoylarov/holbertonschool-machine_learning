d8f0f2027a32ad8d00626490bbbff1d2465755fc
kanangoylarov <goylerov03@gmail.com>
2026-01-21 05:39:03 +0000

    size me pleasse

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """docs"""
+import numpy as np
+
 def matrix_shape(matrix):
     """doc"""
-    return matrix.shape
-
+    matrix1 = np.array(matrix)
+    return matrix1.shape
