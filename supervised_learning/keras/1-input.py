3061673d289077f0877e8863eeb9a92eb67feb8a
Kanan Goylarov <112507822+kanangoylarov@users.noreply.github.com>
2026-03-16 13:29:14 +0400

    input

---
+++ supervised_learning/keras/1-input.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+
+'''Documented
+'''
+import tensorflow.keras as K
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    '''Doc
+    '''
+    model = [K.layers.Input(shape=(nx,))]
+    for n, act in zip(layers, activations):
+        model.append(
+                K.layers.Dense(
+                    n,
+                    activation=act,
+                    kernel_regularizer=K.regularizers.l2(lambtha)
+                )
+        )
+        model.append(K.layers.Dropout(rate=1-keep_prob))
+
+    model.pop()
+    return K.Sequential(model)
